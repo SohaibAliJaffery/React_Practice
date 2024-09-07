f63d0030a0d33d153f19bddf08d4950dcd1ae83c
Sohaib Ali Jaffery <fa22-bce-083@isbstudent.comsats.edu.pk>
2024-09-07 15:16:39 +0500

    Initial commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Avatar from "./Avatar";
+import Detail from "./Detail";
+
+function Card(props) {
+  return (
+    <div className="card">
+      <div className="top">
+        <h2 className="name">{props.name}</h2>
+        <Avatar img={props.img} />
+      </div>
+      <div className="bottom"></div>
+      <Detail tel={props.tel} email={props.email} />
+    </div>
+  );
+}
+
+export default Card;
